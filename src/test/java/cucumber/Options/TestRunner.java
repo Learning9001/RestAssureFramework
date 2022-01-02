8d20d54d01b34b51c19c9ce34911576c58d4a28f
Ram <learning9001@gmail.com>
2022-01-02 11:28:49 -0500

    update

--- src/test/java/cucumber/Options/TestRunner.java
+++ src/test/java/cucumber/Options/TestRunner.java
@@ -8,9 +8,11 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions (	features="src/test/java/features"
 					,glue={"stepDefinations"}
-					,tags= "@All"
+					,tags="@All"
+					,plugin = "json:target/jsonReports/cucumber-report.json"
+					,publish = true
 					)
 public class TestRunner {
 	
-
+	
 }
